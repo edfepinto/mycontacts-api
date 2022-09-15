a7acccfd42929614e7032f5942665296aaf559c6
Eduardo Pinto <edfepinto@gmail.com>
2022-09-15 10:14:35 -0300

    add eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require("express")
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Welcome to MyContacts API! 😎')
-})
+  response.send('Welcome to MyContacts API! 😎');
+});
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
